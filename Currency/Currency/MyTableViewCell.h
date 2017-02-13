e372e93d7f26f0c63d89dd059c4d82c9b35bef95
Igor <igor_xp@mail.ru>
2017-02-14 00:56:51 +0300

    Переделал передачу значения ячейки на делегат

---
+++ Currency/Currency/MyTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewCell.h
+//  Currency
+//
+//  Created by Admin on 09.02.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewCell : UITableViewCell
+
+@end
---
+++ Currency/Currency/MyTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.m
+//  Currency
+//
+//  Created by Admin on 09.02.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import "MyTableViewCell.h"
+
+@implementation MyTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
